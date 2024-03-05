3447109944deb58cb2ea3c1565ddf978fda38790
markwang0 <71631906+markwang0@users.noreply.github.com>
2024-03-04 18:08:47 -0600

    wip

--- examples/wbt_grass_replacement.py
+++ examples/wbt_grass_replacement.py
@@ -1,8 +1,9 @@
+from pathlib import Path
 from whitebox.whitebox_tools import WhiteboxTools
 
 wbt = WhiteboxTools()
-wbt.set_verbose_mode(False)
-wbt.set_working_dir("/home/mark/repos/wip_geoflood/examples/data/")
+wbt.set_verbose_mode(True)  # False suppresses all output
+wbt.set_working_dir(Path(Path(__file__).parent, "data"))
 
 # input
 filtered_dem = "OC1mTest_PM_filtered.tif"
@@ -10,6 +11,7 @@ filtered_dem = "OC1mTest_PM_filtered.tif"
 # outputs
 filtered_filled_dem = "OC1mTest_PM_filtered_filled.tif"
 quinn_mfd_fac = "OC1mTest_PM_filtered_filled_mfd_fac.tif"
+dinf_fac = "OC1mTest_PM_filtered_filled_dinf_fac.tif"
 d8_fdr = "OC1mTest_PM_filtered_filled_d8_fdr.tif"
 basins = "OC1mTest_PM_filtered_filled_d8_fdr_basins.tif"
 
@@ -20,12 +22,21 @@ wbt.fill_depressions(
     # fix_flats=True,
 )
 
-# calculate flow accumulation
+# calculate MFD flow accumulation
 wbt.quinn_flow_accumulation(
     dem=filtered_filled_dem,
     output=quinn_mfd_fac,
     out_type="cells",
 )
+
+# calculate dinf flow accumulation
+wbt.d_inf_flow_accumulation(
+    i=filtered_filled_dem,
+    output=dinf_fac,
+    out_type="cells",
+    pntr=False,
+)
+
 
 # calculate flow direction
 wbt.d8_pointer(
